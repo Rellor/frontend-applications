497e48839cf5bf89a76a3b7ae46daceb53e30c63
Rellor <basderoller@gmail.com>
2021-11-23 15:27:04 +0100

    Followed next.js tutorial (added pages, components and style)

---
+++ components/main/main.js
@@ -0,0 +1,3 @@
+export const Title = ({ children }) => { return <h1 className="App-header"> {children} </h1>;}
+
+export const Footer = ({ children }) => { return <h2 className="App-footer"> {children} </h2>;}
---
+++ components/wordcloud/wordcloudcomp.js
@@ -0,0 +1,16 @@
+import logo from '../../public/assets/logo.svg';
+
+function Wordcloud() {
+  return (
+    <div className="App">
+      <header className="App-body">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Oui Oui React werkt
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Wordcloud;
