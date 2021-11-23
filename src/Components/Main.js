0de837427488a66b271df6f7baef35e5b7707953
Rellor <basderoller@gmail.com>
2021-11-23 11:18:41 +0100

    Begonnen met next.js

--- src/Components/Main.js
+++
@@ -1,5 +0,0 @@
-import "../App.css";
-
-export const Title = ({ children }) => { return <h1 className="App-header"> {children} </h1>;}
-
-export const Footer = ({ children }) => { return <h2 className="App-footer"> {children} </h2>;}
